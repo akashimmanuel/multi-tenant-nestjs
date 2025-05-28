83beee9c4b730c5ae94de03a4be64ce05ad04b1c
bhaskar1008 <bhaskarkeelu.92@gmail.com>
2025-05-28 20:33:57 +0530

    added allowedOrigin config

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['http://localhost:5173'];
   app.enableCors({
-    origin: '*',
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-tenant-id'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-tenant-id', 'Authorization'],
     credentials: true,
   });
+
   await app.listen(3000);
 }
 bootstrap();
